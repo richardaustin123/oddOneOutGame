c9f0bba8b431ac11220708ecc5c0c56afd9d5bad
Richard Austin <richardaustin123@icloud.com>
2022-04-30 16:28:29 +0100

    heroku connetion

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
-    const socket = io("http://localhost:3000/");
-// const socket = io("https://oddoneoutgame.herokuapp.com/");
+    // const socket = io("http://localhost:3000/");
+const socket = io("https://oddoneoutgame.herokuapp.com/");
 
 console.log("server started");
 
